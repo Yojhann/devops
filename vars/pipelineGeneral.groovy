ab05732b9df3968c3e135b033370148a0a7f3434
Yojhann <140549098+Yojhann@users.noreply.github.com>
2024-05-02 23:40:06 -0500

    Update pipelineGeneral.groovy

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,43 +1,39 @@
 def call(){
-
     pipeline{
-
         agent any
-
         tools{
             nodejs 'NodeJS18'
         }
-
-        // triggers {
-        //     pollSCM('* * * * *') // Programa la verificación del repositorio cada minuto
-        // }
-
         environment{
-            projectName = "${env.GIT_URL_1}".replaceAll('.+/(.+)\\.git', '$1')toLowerCase()
+            projectName = "${env.UrlGitHub}".replaceAll('.+/(.+)\\.git', '$1')toLowerCase()
         }
-
         stages{
-            stage('Proceso Construccion') {
+            stage('Fase 2: Construcción de imagen en Docker Desktop') {
                 steps {
                     script {
-                        def buildapp = new org.devops.lb_buildartefacto()
-                        buildapp.install()
-                        def cloneapp = new org.devops.lb_buildartefacto()
-                        cloneapp.clone()
+                        def buildimage new org.devops.lb_buildimagen()
+                        buildimage.buildImageDocker("${projectName}")
                     }
                 }
             }
+            stage('Fase 2: publicar imagen a docker hub') {
+                steps {
+                    script {
+                        def publicImagenew org.devops.lb_publicardockerhub()
+                        publicImage.publicarImage("${projectName}")
+                    }
+                }
+            }
+            
+            stage('Fase 2: Desplegar imagen en docker') {
+                steps {
+                    script{
+                        def deployImg = new org.devops.lb_deploydocker()
+                        deployImg.despliegueContenedor ("$(projectName)")
+                        }
+                    }
+                }
 
-            stage('Analisis Sonarqube ') {
-                steps {
-                    script {
-                        def test = new org.devops.lb_analisissonarqube()
-                        test.runTest()
-                        def analisysSonarqube = new org.devops.lb_analisissonarqube()
-                        analisysSonarqube.analisys("${projectName}")
-                    }
-                }
-            }
         }
     }
 }
