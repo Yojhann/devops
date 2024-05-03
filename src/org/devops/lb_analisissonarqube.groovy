a7b9989758012979ac1937da69f1364a73daac40
Yojhann <140549098+Yojhann@users.noreply.github.com>
2024-05-02 20:53:36 -0500

    Update lb_analisissonarqube.groovy

--- src/org/devops/lb_analisissonarqube.groovy
+++ src/org/devops/lb_analisissonarqube.groovy
@@ -1,20 +1,20 @@
 package org.devops
-def testCoverage(){
+def runTest(){
     sh 'npm test' 
 }
 
-def analisisSonar (gitName) {
+def analisys (projectGitName) {
     def scannerHome = tool 'sonar-scanner'
         if (scannerHome) {
             withSonarQubeEnv('sonar-scanner'){
-            sh "${scannerHome}/bin/sonar-scanner \
-            -Dsonar.projectKey=${gitName} \
-            -Dsonar.projectName=${gitName} \
-            -Dsonar.sources=${env.source} \
-            -Dsonar.tests=src/__test__\
-            -Dsonar.exclusions='**/*.test.js' \
-            -Dsonar.testExecutionReportPaths=./test-report.xml \
-            -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info"
+                sh "${scannerHome}/bin/sonar-scanner \
+                    -Dsonar.projectKey=${gitName} \
+                    -Dsonar.projectName=${gitName} \
+                    -Dsonar.sources=${env.source} \
+                    -Dsonar.tests=src/__test__\
+                    -Dsonar.exclusions='**/*.test.js' \
+                    -Dsonar.testExecutionReportPaths=./test-report.xml \
+                    -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info"
         }
     } else{
     error 'SonarQube Scanner not found'
