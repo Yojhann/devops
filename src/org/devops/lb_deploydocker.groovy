0c3fa1017b3aef78dba447fe18d0c6f39272c30c
Yojhann <140549098+Yojhann@users.noreply.github.com>
2024-05-03 01:31:42 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -1,7 +1,7 @@
 package org.devops
 
 def despliegueContenedor(projectGitName) {
-    sh "docker pull ${env.DOCKERHUB_USERNAME}/${projectGitName}"
+    sh "docker pull yojhancristancho/react-test:latest"
     sh """ docker run -d --name ${projectGitName} \
     --network=${env.NameNetwork} -p 5174:5174 \
     --user root yojhancristancho/react-test:latest
