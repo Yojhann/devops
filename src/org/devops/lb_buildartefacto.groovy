c693e4f71b1869d261bf0620d9653b221f4890f1
Yojhann <140549098+Yojhann@users.noreply.github.com>
2024-05-02 22:11:41 -0500

    Update lb_buildartefacto.groovy

--- src/org/devops/lb_buildartefacto.groovy
+++ src/org/devops/lb_buildartefacto.groovy
@@ -1,10 +1,10 @@
 package org.devops
 
-def install(){
-    sh 'npm install'
-}
-
 def clone(){
     git branch: "${env.GIT_BRANCH_1}", url: "${env.GIT_URL_1}"
+}
+
+def install(){
+    sh 'npm install'
 }
 
