d56885190236fdea4b4e6f019e7c095a479ee823
Yojhann <140549098+Yojhann@users.noreply.github.com>
2024-05-03 00:30:54 -0500

    Update lb_publicardockerhub.groovy

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -1,10 +1,15 @@
 package org.devops
 
-def publicarImage (projectGitName) {
-    withCredentials ([usernamePassword(credentialsId: "${env.TOKEN_ID}",
-    passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
-    sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
-    sh "docker tag ${projectGitName} ${env.DOCKERHUB_USERNAME}/${projectGitName}"
-    sh "docker push ${env.DOCKERHUB_USERNAME}/${projectGitName}"
+def publicarImage(projectGitName) {
+    withCredentials([
+        usernamePassword(
+            credentialsId: "${env.TOKEN_ID}",
+            passwordVariable: 'DOCKERHUB_PASSWORD',
+            usernameVariable: 'DOCKERHUB_USERNAME'
+        )
+    ]) {
+        sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
+        sh "docker tag ${projectGitName} yojhancristancho/${projectGitName}"
+        sh "docker push yojhancristancho/${projectGitName}"
     }
 }
